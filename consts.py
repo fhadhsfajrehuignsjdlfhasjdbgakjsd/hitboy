d7ee6baf8a4d5803287b82eef3953e6bd6b8fe06
solemn-leader <solemnda@gmail.com>
2019-02-24 11:25:58 +1000

    Playable

--- consts.py
+++ consts.py
@@ -1,6 +1,7 @@
 # general:
 SCREEN_SIZE = (1000, 550)  # pygame display size
 FLOOR_RECT_POSITION = (0, 500, 1000, 50)  # x, y, width, height
+POINTER_SIZE = (20, 20)
 ABOUT_URL = 'https://github.com/solemn-leader/hitboy/blob/master/README.md'
 # colors:
 BLUE = (173, 216, 230)
--- main.py
+++ main.py
@@ -1,116 +1,137 @@
 import pygame
-import webbrowser
 from consts import *
-import threading
 
 pygame.init()
 pygame.font.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
-from helpers import *
-from entities import (
-    entities, 
-    obstacles, 
-    menu, 
-    score,
-    pause_menu,
-    start_new_game,
-    obstacle_adder
-)
 
-timer = pygame.time.Clock()
-# pygame.mixer.init()
-# pygame.mixer.music.load("audio/monster.mp3")
-# pygame.mixer.music.play()
-game_status = GameStatuses.MENU
-max_score = get_max_score()
-pygame.display.set_caption('Hitboy.')
-pygame.display.set_icon(load_image('favicon.png'))
+def main():
+    from entities import (
+        entities,
+        obstacles,
+        menu,
+        score,
+        pause_menu,
+        start_new_game,
+        obstacle_adder
+    )
+    from helpers import (
+        get_max_score,
+        load_image,
+        clean_screen,
+        check_exit,
+        check_pause,
+        get_user_choice_menu,
+        up_button_clicked
+    )
+    import webbrowser
+    timer = pygame.time.Clock()
+    # pygame.mixer.init()
+    # pygame.mixer.music.load("audio/monster.mp3")
+    # pygame.mixer.music.play()
+    game_status = GameStatuses.MENU
+    max_score = get_max_score()
+    pygame.display.set_caption('Hitboy.')
+    pygame.display.set_icon(load_image('favicon.png'))
+    pointer = pygame.transform.scale(load_image('pointer.png'), POINTER_SIZE)
 
-while True:
-    time_passed_in_secs = timer.tick(60) / 1000
-    if game_status == GameStatuses.GAME_OVER:
-        menu.draw(screen)
-        events = pygame.event.get()
-        if check_exit(events):
-            pygame.quit()
-            exit(0)
-        user_choice = menu.get_user_choice(events)
-        if user_choice is not None:
-            if user_choice == UserChoicesMenu.PLAY:
-                game_status = GameStatuses.PLAYING
-                entities, obstacles = start_new_game(entities, obstacles, obstacle_adder, score)                
-            elif user_choice == UserChoicesMenu.EXIT:
+    while True:
+        time_passed_in_secs = timer.tick(60) / 1000
+        if game_status == GameStatuses.GAME_OVER:
+            menu.draw(screen)
+            events = pygame.event.get()
+            if check_exit(events):
                 pygame.quit()
                 exit(0)
-            elif user_choice == UserChoicesMenu.ABOUT:
-                webbrowser.open(ABOUT_URL)
+            user_choice = menu.get_user_choice(events)
+            if user_choice is not None:
+                if user_choice == UserChoicesMenu.PLAY:
+                    game_status = GameStatuses.PLAYING
+                    pygame.mouse.set_visible(False)
+                    entities, obstacles = start_new_game(
+                        entities, obstacles, obstacle_adder, score)
+                elif user_choice == UserChoicesMenu.EXIT:
+                    pygame.quit()
+                    exit(0)
+                elif user_choice == UserChoicesMenu.ABOUT:
+                    webbrowser.open(ABOUT_URL)
 
-    elif game_status == GameStatuses.PAUSE:
-        pause_menu.draw(screen)
-        events = pygame.event.get()
-        if check_exit(events):
-            pygame.quit()
-            exit(0)
-        user_choice = pause_menu.get_user_choice(events)
-        if user_choice is not None:
-            if user_choice == UserChoicesMenu.PLAY:
-                game_status = GameStatuses.PLAYING
-            elif user_choice == UserChoicesMenu.EXIT:
+        elif game_status == GameStatuses.PAUSE:
+            pause_menu.draw(screen)
+            events = pygame.event.get()
+            if check_exit(events):
                 pygame.quit()
                 exit(0)
-            elif user_choice == UserChoicesMenu.RESTART:
-                game_status = GameStatuses.PLAYING
-                entities, obstacles = start_new_game(entities, obstacles, obstacle_adder, score)
-            if user_choice == UserChoicesMenu.PLAY:
-                    game_status = GameStatuses.PLAYING            
+            user_choice = pause_menu.get_user_choice(events)
+            if user_choice is not None:
+                if user_choice == UserChoicesMenu.PLAY:
+                    game_status = GameStatuses.PLAYING
+                    pygame.mouse.set_visible(False)
+                elif user_choice == UserChoicesMenu.EXIT:
+                    pygame.quit()
+                    exit(0)
+                elif user_choice == UserChoicesMenu.RESTART:
+                    game_status = GameStatuses.PLAYING
+                    entities, obstacles = start_new_game(
+                        entities, obstacles, obstacle_adder, score)
+                    pygame.mouse.set_visible(False)
 
-    elif game_status == GameStatuses.MENU:
-        clean_screen(screen, game_status)
-        menu.draw(screen)
-        events = pygame.event.get()
-        if check_exit(events):
-            pygame.quit()
-            exit(0)
-        user_choice = menu.get_user_choice(events)
-        if user_choice is not None:
-            if user_choice == UserChoicesMenu.PLAY:
-                game_status = GameStatuses.PLAYING
-                entities, obstacles = start_new_game(entities, obstacles, obstacle_adder, score)
-            elif user_choice == UserChoicesMenu.EXIT:
+        elif game_status == GameStatuses.MENU:
+            clean_screen(screen, game_status)
+            menu.draw(screen)
+            events = pygame.event.get()
+            if check_exit(events):
                 pygame.quit()
                 exit(0)
-            elif user_choice == UserChoicesMenu.ABOUT:
-                webbrowser.open(ABOUT_URL)
+            user_choice = menu.get_user_choice(events)
+            if user_choice is not None:
+                if user_choice == UserChoicesMenu.PLAY:
+                    game_status = GameStatuses.PLAYING
+                    pygame.mouse.set_visible(False)
+                    entities, obstacles = start_new_game(
+                        entities, obstacles, obstacle_adder, score)
+                elif user_choice == UserChoicesMenu.EXIT:
+                    pygame.quit()
+                    exit(0)
+                elif user_choice == UserChoicesMenu.ABOUT:
+                    webbrowser.open(ABOUT_URL)
 
-    elif game_status == GameStatuses.PLAYING:
-        events = pygame.event.get()
-        if check_pause(events):
-            game_status = GameStatuses.PAUSE
-            continue
-        if check_exit(events):
-            pygame.quit()
-            exit(0)
-        clean_screen(screen, GameStatuses.PLAYING)
-        score.increase()
-        score.draw(screen)
-        obstacle_adder.add_obstacle_if_necessary(entities, obstacles)
-        for entity in entities:
-            if entity.is_movable:
-                entity.move(
-                    time_passed_in_secs,
-                    up_button_clicked=up_button_clicked(events)
-                )
-            if entity.can_die:
-                dead = entity.try_to_die(obstacles)
-                if dead:
+        elif game_status == GameStatuses.PLAYING:
+            events = pygame.event.get()
+            if check_pause(events):
+                game_status = GameStatuses.PAUSE
+                pygame.mouse.set_visible(True)
+                continue
+            if check_exit(events):
+                pygame.quit()
+                exit(0)
+            clean_screen(screen, GameStatuses.PLAYING)
+            score.increase()
+            score.draw(screen)
+            obstacle_adder.add_obstacle_if_necessary(entities, obstacles)
+            for entity in entities:
+                if entity.is_movable:
+                    entity.move(
+                        time_passed_in_secs,
+                        up_button_clicked=up_button_clicked(events)
+                    )
+                if entity.can_die:
+                    dead = entity.try_to_die(obstacles)
+                    if dead:
+                        entity.draw(screen)
+                        score.check_new_max_score()
+                        pygame.display.flip()
+                        game_status = GameStatuses.GAME_OVER
+                        pygame.mouse.set_visible(True)
+                        break
+                if entity.must_be_deleted:
+                    entity.delete()
+                else:
                     entity.draw(screen)
-                    score.check_new_max_score()
-                    pygame.display.flip()
-                    game_status = GameStatuses.GAME_OVER
-                    break
-            if entity.must_be_deleted:
-                entity.delete()
-            else:
-                entity.draw(screen)
-    pygame.display.flip()
+            screen.blit(pointer, pygame.mouse.get_pos())
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
